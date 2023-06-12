cbaa72c5cac8e41f1b5f01c1d1e43b93007eb4fa
abood alomary <abboodoa@gmail.com>
2023-06-12 16:09:19 +0300

    all done and ready for push

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace NumberGame
+﻿using System;
+
+namespace NumberGame
 {
     internal class Program
     {
@@ -6,111 +8,139 @@
         {
             try
             {
+                // Call the StartSequence method
                 StartSequence();
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"You did something wrong {ex}");
+                // When the user enters something wrong
+                Console.WriteLine($"You did something wrong: {ex}");
             }
             finally
             {
+                // Finally block will always appear
                 Console.WriteLine("Program is completed");
             }
-
-
         }
+
         static void StartSequence()
         {
             try
             {
-                Console.WriteLine("Enter a number greater than zero");
-                //int size = Convert.ToInt32(Console.ReadLine());
+                // Ask the user to enter the size of the array
+                Console.WriteLine("Enter the size of the array (the number should be greater than zero): ");
                 int size = Int32.Parse(Console.ReadLine());
+
+                // Initialize the numbers array with the given size
                 int[] numbers = new int[size];
+
+                // Check if the size is 1 because we have methods using more than one element in the array
                 if (size == 1)
                 {
-                    throw new Exception("The array size connot be 1 Only!");
-
+                    // Throw an exception to the user
+                    throw new Exception("The array size cannot be 1!");
                 }
+
+                // Call the Populate method to fill the array with user input
                 numbers = Populate(numbers);
+
+                // Calculate the sum of the array elements
                 int sum = GetSum(numbers);
+
+                // Get a random index from the user and calculate the product
                 int Product = GetProduct(numbers, sum);
+
+                // Calculate the quotient by dividing the product by a user-entered number
                 decimal Quotient = GetQuotient(Product);
-                Console.WriteLine($"Your array size {size}");
-                Console.Write("Your Array element is : ");
+
+                // Print the size of the array
+                Console.WriteLine($"Your array size is: {size}");
+
+                // Print the array elements
+                Console.Write("Your Array elements are: ");
                 for (int i = 0; i < numbers.Length; i++)
                 {
                     Console.Write(numbers[i] + " ");
                 }
                 Console.WriteLine();
-                Console.WriteLine($"the sum of array is {sum}");
-                Console.WriteLine($"{sum} * {Product / sum} = {Product} ");
-                Console.WriteLine($" {Product} / {Product / Quotient} = {Quotient}");
+
+                // Print the sum of the array
+                Console.WriteLine($"The sum of the array is: {sum}");
+
+                // Print the product of the sum and the random index
+                Console.WriteLine($"{sum} * {Product / sum} = {Product}");
+
+                // Print the quotient of the product divided by the user input
+                Console.WriteLine($"{Product} / {Product / Quotient} = {Quotient}");
             }
             catch (FormatException ex)
             {
-                Console.WriteLine($"You entered not an integer  {ex}");
+                Console.WriteLine($"You entered a non-integer value: {ex}");
             }
             catch (OverflowException ex)
             {
-                Console.WriteLine($"You inputed a huge Number {ex}");
+                Console.WriteLine($"You entered a huge number: {ex}");
             }
-
         }
+
         static int[] Populate(int[] numbers)
         {
-            string inputString = "";
-            int inputNumber = 0;
             for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine($"Please enter a number {i + 1} of {numbers.Length}");
-                inputString = Console.ReadLine();
-                inputNumber = Int32.Parse(inputString);
+                Console.WriteLine($"Please enter number {i + 1} of {numbers.Length}: ");
+                string inputString = Console.ReadLine();
+                int inputNumber = Int32.Parse(inputString);
                 numbers[i] = inputNumber;
-                Console.WriteLine($"You Enterd number :  {numbers[i]}");
+                Console.WriteLine($"You entered number: {numbers[i]}");
             }
+
             return numbers;
         }
+
         static int GetSum(int[] numbers)
         {
             int sum = 0;
+
             foreach (int number in numbers)
             {
                 sum += number;
             }
+
             if (sum < 20)
             {
-                throw new Exception($"Value of sum: {sum} in array is too low! ");
+                throw new Exception($"Value of sum ({sum}) in the array is too low!");
             }
-
 
             return sum;
         }
 
         static int GetProduct(int[] numbers, int sum)
         {
+            Console.WriteLine($"Please select a random number from 1 to {numbers.Length}: ");
+            int randomIndex = Int32.Parse(Console.ReadLine()) - 1;
 
-            Console.WriteLine($"Please Select Random Number  From 1 to {numbers.Length}");
-            int randomIndex = Int32.Parse(Console.ReadLine()) -1;
             if (randomIndex < 0 || randomIndex >= numbers.Length)
             {
-                throw new IndexOutOfRangeException("You inputed number which is not index in array and it is out of Range");
+                throw new IndexOutOfRangeException("You entered a number that is not a valid index in the array.");
             }
+
             int product = sum * numbers[randomIndex];
             return product;
         }
+
         static decimal GetQuotient(int product)
         {
-            Console.WriteLine($"enter a number to divide the product :{product} by!");
-            int divide = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Enter a number to divide the product ({product}) by: ");
+            int divide = Int32.Parse(Console.ReadLine());
+
             if (divide == 0)
             {
-                Console.WriteLine("You Divided by Zero");
+                Console.WriteLine("You divided by zero.");
                 return 0;
             }
+
             decimal quotient = decimal.Divide(product, divide);
             return quotient;
         }
-
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # NumberGame
+## How To use clone repositories:
+- ### To use my application make sure you have .net core and the version i used is 7.0 
+- ### in this repo copy the `https` for my repo from `code` button and then choose `https` and copy it
+- ### once you run visual studio you can see `clone repositories` click on it  
+- ### now you will see a new window and you can see location put here the `https` that you copy before
+- ### and you can see the path put where you want it to clone 
+- ### and you good to go 
+- ### if you already in visual studio you can see `git` bar from nav bar and inside it you can see the `clone repositories`
+- ### and you have to repeat the last above steps.
+
+## Questions : 
+- ### What is the purpose of the program?
+	- #### well this program has is an mathematic app which you notice from the name, the program will allows you to enter multi number and store them in array by a size of you chocie and then get sum of the array, getProduct, get Quointent, and all of these work by your choice.
+- ### How do i run the program
+	-  #### press `F5` or in top of you visual studio you can see `debug` bar click and you can see `start Debugging` click on in and boom you run the program
+		- #### so what to do and how to use it it is easy : 
+	      ##### just follow the instruction in the console and you good to go 
+		  ##### remember to put only integer numbers
+- ### what does the program look like
+!(visual view for my output)[numbersgame.png]
